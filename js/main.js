c43f44a598ec550aac44074d1603d65fe109efae
anitarazafi <anitarazafindrakotovao@gmail.com>
2023-01-08 18:34:24 +0300

    Added animation in navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+const header = document.getElementById("header");
+function fixNav() {
+  if (window.scrollY > header.offsetHeight + 150) {
+    header.classList.add("sticky-top");
+  } else {
+    header.classList.remove("sticky-top");
+  }
+}
+window.addEventListener("scroll", fixNav);
